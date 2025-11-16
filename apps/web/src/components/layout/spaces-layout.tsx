6033be9f0cfbd8211e907d2e8841d2b401cb9630
DevAbabil <ababilhossain35@gmail.com>
2025-11-16 18:31:04 +0600

    feat(layout): update space switcher and layout components

--- apps/web/src/components/layout/spaces-layout.tsx
+++ apps/web/src/components/layout/spaces-layout.tsx
@@ -28,7 +28,6 @@ const SpacesLayout: FC<PropsWithChildren> = ({ children }) => {
           </InputGroup>
         </header>
         {children}
-
         <VerifyUniversityEmail />
       </SidebarInset>
     </SidebarProvider>
