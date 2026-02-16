4bf6fe494165f1c26c6ff30de09211bd7590c3d4
ItsMoloy <moloy21@student.sust.edu>
2026-02-17 05:12:17 +0600

    personal,events,network

--- apps/web/src/routes/spaces/societies/discover/network.tsx
+++ apps/web/src/routes/spaces/societies/discover/network.tsx
@@ -1,5 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Network from '@web/components/clubs/discover/network'
+
+const NetworkPage = () => (
+  <div className="mx-auto w-full max-w-5xl px-4 py-6 sm:px-6 lg:px-8">
+    <Network />
+  </div>
+)
 
 export const Route = createFileRoute('/spaces/societies/discover/network')({
-  component: () => null
+  component: NetworkPage
 })
+
+export default NetworkPage
