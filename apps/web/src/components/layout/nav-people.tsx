e868f63d9db099839ade764e48fd24beece80bad
ItsMoloy <moloy21@student.sust.edu>
2026-02-23 15:42:40 +0600

    component

--- apps/web/src/components/layout/nav-people.tsx
+++ apps/web/src/components/layout/nav-people.tsx
@@ -1,3 +1,31 @@
+// 'use client'
+
+// import {
+//   SidebarMenu,
+//   SidebarMenuButton,
+//   SidebarMenuItem
+// } from '@rov/ui/components/sidebar'
+// import { Link, useLocation } from '@tanstack/react-router'
+// import { Users } from 'lucide-react'
+
+// export function NavPeople() {
+//   const location = useLocation()
+//   const isActive = location.pathname === '/people'
+
+//   return (
+//     <SidebarMenu>
+//       <SidebarMenuItem>
+//         <SidebarMenuButton isActive={isActive} tooltip="People">
+//           <Link to="/people">
+//             <Users className="h-4 w-4" />
+//             <span>People</span>
+//           </Link>
+//         </SidebarMenuButton>
+//       </SidebarMenuItem>
+//     </SidebarMenu>
+//   )
+// }
+
 'use client'
 
 import {
@@ -16,7 +44,7 @@ export function NavPeople() {
     <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton isActive={isActive} tooltip="People">
-          <Link to="/people">
+          <Link params={{}} to="/people">
             <Users className="h-4 w-4" />
             <span>People</span>
           </Link>
